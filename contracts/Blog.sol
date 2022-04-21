8d2cb8e019ead542873f96ed287cb3576bdfe829
Drew Morrison <drewm1128@gmail.com>
2022-04-21 13:30:21 -0500

    Created the header with a connect button to using web3 Modal. Set up useContect to share state globaly within the app

--- contracts/Blog.sol
+++ contracts/Blog.sol
@@ -12,73 +12,83 @@ contract Blog {
     Counters.Counter private _postIds;
 
     struct Post {
-        uint id;
-        string title;
-        string content; // This is going to be the IPFS hash where the content is stored
-        bool published;
+      uint id;
+      string title;
+      string content;
+      bool published;
     }
+    /* mappings can be seen as hash tables */
+    /* here we create lookups for posts by id and posts by ipfs hash */
+    mapping(uint => Post) private idToPost;
+    mapping(string => Post) private hashToPost;
 
-    mapping(uint => Post) public idToPosts;
-    mapping(string => Post) public hashToPosts;
-
+    /* events facilitate communication between smart contractsand their user interfaces  */
+    /* i.e. we can create listeners for events in the client and use them in The Graph  */
     event PostCreated(uint id, string title, string hash);
     event PostUpdated(uint id, string title, string hash, bool published);
 
-   constructor(string memory _name) {
-       console.log("Deploying blog with name: ",  _name);
+    /* when the blog is deployed, give it a name */
+    /* also set the creator as the owner of the contract */
+    constructor(string memory _name) {
+        console.log("Deploying Blog with name:", _name);
         name = _name;
         owner = msg.sender;
-   }
+    }
 
-   function updateName(string memory _name) public {
-       name = _name;
-   }
+    function updateName(string memory _name) public {
+        name = _name;
+    }
 
     function transferOwnership(address newOwner) public onlyOwner {
         owner = newOwner;
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
+    /* fetches an individual post by the content hash */
+    function fetchPost(string memory hash) public view returns(Post memory){
+      return hashToPost[hash];
     }
 
-    function fetchPost( string memory hash) public view returns (Post memory) {
-        return hashToPosts[hash];
-    }
-
+    /* creates a new post */
     function createPost(string memory title, string memory hash) public onlyOwner {
         _postIds.increment();
         uint postId = _postIds.current();
-        Post storage post = idToPosts[postId];
+        Post storage post = idToPost[postId];
         post.id = postId;
         post.title = title;
         post.published = true;
         post.content = hash;
-        hashToPosts[hash] = post;
+        hashToPost[hash] = post;
         emit PostCreated(postId, title, hash);
     }
 
+    /* updates an existing post */
     function updatePost(uint postId, string memory title, string memory hash, bool published) public onlyOwner {
-        Post storage post = idToPosts[postId];
+        Post storage post =  idToPost[postId];
         post.title = title;
         post.published = published;
         post.content = hash;
-        hashToPosts[hash] = post;
+        idToPost[postId] = post;
+        hashToPost[hash] = post;
         emit PostUpdated(post.id, title, hash, published);
     }
 
+    /* fetches all posts */
     function fetchPosts() public view returns (Post[] memory) {
         uint itemCount = _postIds.current();
 
         Post[] memory posts = new Post[](itemCount);
         for (uint i = 0; i < itemCount; i++) {
             uint currentId = i + 1;
-            Post storage currentItem = idToPosts[currentId];
+            Post storage currentItem = idToPost[currentId];
             posts[i] = currentItem;
         }
         return posts;
     }
 
-    
+    /* this modifier means only the contract owner can */
+    /* invoke the function */
+    modifier onlyOwner() {
+      require(msg.sender == owner);
+    _;
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,170 @@
+import { useState } from 'react'
+import Link from 'next/link'
+import { css } from '@emotion/css'
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { AccountContext } from '../context.js'
+import { ownerAddress } from '../config'
+import 'easymde/dist/easymde.min.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [account, setAccount] = useState(null)
+
+  async function getWeb3Modal() {
+    const web3Modal = new Web3Modal({
+      cacheProvider: false,
+      providerOptions: {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: { 
+            infuraId: process.env.INFURA_ID
+          },
+        },
+      },
+    })
+    return web3Modal
+  }
+
+  async function connect() {
+    try {
+      const web3Modal = await getWeb3Modal()
+      const connection = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection)
+      const accounts = await provider.listAccounts()
+      setAccount(accounts[0])
+    } catch (err) {
+      console.log('error:', err)
+    }
+  }
+
+  return (
+    <div>
+      <nav className={nav}>
+        <div className={header}>
+          <Link href="/">
+            <a>
+              <img
+                src='/logo.svg'
+                alt="Super Cool Logo"
+                style={{ width: '50px' }}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <div className={titleContainer}>
+                <h2 className={title}>Full Stack</h2>
+                <p className={description}>WEB3</p>
+              </div>
+            </a>
+          </Link>
+          {
+            !account && (
+              <div className={buttonContainer}>
+                <button className={buttonStyle} onClick={connect}>Connect</button>
+              </div>
+            )
+          }
+          {
+            account && <p className={accountInfo}>{account}</p>
+          }
+        </div>
+        <div className={linkContainer}>
+          <Link href="/" >
+            <a className={link}>
+              Home
+            </a>
+          </Link>
+          {
+           
+            (account === ownerAddress) && (
+              <Link href="/create-post">
+                <a className={link}>
+                  Create Post
+                </a>
+              </Link>
+            )
+          }
+        </div>
+      </nav>
+      
+      <div className={container}>
+        <AccountContext.Provider value={account}>
+          <Component {...pageProps} connect={connect} />
+        </AccountContext.Provider>
+      </div>
+    </div>
+  )
 }
+
+const accountInfo = css`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+  font-size: 12px;
+`
+
+const container = css`
+  padding: 40px;
+`
+
+const linkContainer = css`
+  padding: 30px 60px;
+  background-color: #fafafa;
+`
+
+const nav = css`
+  background-color: white;
+`
+
+const header = css`
+  display: flex;
+  border-bottom: 1px solid rgba(0, 0, 0, .075);
+  padding: 20px 30px;
+`
+
+const description = css`
+  margin: 0;
+  color: #999999;
+`
+
+const titleContainer = css`
+  display: flex;
+  flex-direction: column;
+  padding-left: 15px;
+`
+
+const title = css`
+  margin-left: 30px;
+  font-weight: 500;
+  margin: 0;
+`
+
+const buttonContainer = css`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`
+
+const buttonStyle = css`
+  background-color: #fafafa;
+  outline: none;
+  border: none;
+  font-size: 18px;
+  padding: 16px 70px;
+  border-radius: 15px;
+  cursor: pointer;
+  box-shadow: 7px 7px rgba(0, 0, 0, .1);
+`
+
+const link = css`
+  margin: 0px 40px 0px 0px;
+  font-size: 16px;
+  font-weight: 400;
+`
 
 export default MyApp
