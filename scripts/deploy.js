d8f7e8bfe5c4bfc42179cca987a930163d44bccf
Drew Morrison <drewm1128@gmail.com>
2022-04-21 11:00:07 -0500

    Deployed contract using hardhat. I also used the fs whithin the script to create a config file and store the contract and owner address

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+ 
+  const Blog = await hre.ethers.getContractFactory("Blog");
+  const blog = await Blog.deploy("My web3 blog");
+
+  await blog.deployed();
+  console.log("Blog deployed to:", blog.address);
+
+  fs.writeFileSync('./config.js', `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${blog.signer.address}"
+  `);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
