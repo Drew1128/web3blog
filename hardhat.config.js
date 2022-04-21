a2224e1fc3a04ae98cdab71146acd6b2f5bf76cc
Drew Morrison <drewm1128@gmail.com>
2022-04-21 08:54:48 -0500

    Configured hardhat and added three networks in the the hardhat config file

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle");
+
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+   hardhat: {
+     chainId: 1337,
+   },
+   mumbai: {
+     url: "https://rpc-mumbai.matic.today",
+     accounts: [process.env.pkey],
+   },
+   ploygon: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.pkey],
+   },
+  },
+};
